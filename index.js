92542e3abec53dc78bb8d5a420584dce61dab3cb
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-03 22:39:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key]=value;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-
+  object[key]=value;
 }
 
 function deleteFromObjectByKey(object, key){
